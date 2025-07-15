0be2718ffeb2b382e239f2a9dc0c11f1b5c91b5a
Ram <ramprasadk874@gmail.com>
2025-07-15 13:25:13 +0530

    Modified Comments

--- src/test/java/com/swagLabs/Qa/TestCases/CartClass.java
+++ src/test/java/com/swagLabs/Qa/TestCases/CartClass.java
@@ -9,6 +9,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.aventstack.chaintest.plugins.ChainTestListener;
 import com.swagLabs.Qa.TestBase.TestBase;
 import com.swagLabs.Qa.Utils.Input;
 
@@ -20,7 +21,7 @@ public class CartClass extends TestBase{
 	{
 		Input.setting_Up_BrowserName(bName);
 		Input.urlNavigation(browserURL);
-		System.out.println("Going to Enter Valid Login Username and Password.");
+		ChainTestListener.log("Going to Enter Valid Login Username and Password.");
 		Input.textEnter(userNameField(), userName_New);
 		Input.textEnter(passwordField(), password_New);
 		Input.clickOn(loginButton(), click);
@@ -33,14 +34,13 @@ public class CartClass extends TestBase{
 	
 		if(Input.getDriver().getTitle().contains(inventoryPageTitle)) 
 		{
-			System.out.println("Page Title is Matched : "  +  inventoryPageTitle);
+			ChainTestListener.log("Page Title is Matched : "  +  inventoryPageTitle);
 		}else 
 		{
-			System.out.println("Unable to Find Page Title : "  +  Input.getDriver().getTitle());
+			ChainTestListener.log("Unable to Find Page Title : "  +  Input.getDriver().getTitle());
 			Assert.assertTrue(false);
 		}
-		
-		System.out.println("Going to Add 6 Items To Cart.");
+		ChainTestListener.log("Going to Add 6 Items To Cart.");
 		 List<WebElement> allItems = Input.getDriver().findElements(addTocart());
 		TimeUnit.SECONDS.sleep(1);
 		for(int i=0;i<allItems.size();i++) 
@@ -48,9 +48,9 @@ public class CartClass extends TestBase{
 			allItems.get(i).click();
 			
 		}
-		System.out.println("Number of Items Added to cart : " + allItems.size());
+		ChainTestListener.log("Number of Items Added to cart : " + allItems.size());
 		TimeUnit.SECONDS.sleep(1);
-		System.out.println("Going to remove all 6 items from Shopping Cart.");
+		ChainTestListener.log("Going to remove all 6 items from Shopping Cart.");
 		TimeUnit.SECONDS.sleep(1);
 		Input.clickOn(shopping_Cart_Badge(), click);
 		TimeUnit.SECONDS.sleep(1);
@@ -60,7 +60,8 @@ public class CartClass extends TestBase{
 		{
 			removeItems.get(i).click();
 		}
-		System.out.println("Number of Items Removed from  cart : " + removeItems.size());
+		ChainTestListener.log("Number of Items Removed from  cart : " + removeItems.size());
+		System.out.println();
 		Input.clickOn(continueShopping(), click);
 		TimeUnit.SECONDS.sleep(1);
 		Input.clickOn(openMenu(), click);
@@ -80,7 +81,7 @@ public class CartClass extends TestBase{
 		{
 			allItems.get(i).click();
 		}
-		System.out.println("Going to perform Checkout.");
+		ChainTestListener.log("Going to perform Checkout.");
 		TimeUnit.SECONDS.sleep(1);
 		Input.clickOn(shopping_Cart_Badge(), click);
 		Input.clickOn(checkOut(), click);
@@ -92,10 +93,10 @@ public class CartClass extends TestBase{
 		Input.clickOn(finishButton(), click);
 		if(Input.getElementText(thanksForOrder()).contains(thanksForOrderText)) 
 		{
-			System.out.println("Your order has been dispatched, and will arrive just as fast as the pony can get there!");
+			ChainTestListener.log("Your order has been dispatched, and will arrive just as fast as the pony can get there!");
 		}else
 		{
-			System.out.println("Failed to Find the Element.");
+			ChainTestListener.log("Failed to Find the Element.");
 			Assert.assertTrue(false);
 		}
 		
